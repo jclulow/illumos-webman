67789ad751799fdec750ef5d5a310ffc7764474b
Joshua M. Clulow <josh@sysmgr.org>
2012-09-14 09:22:41 +0000

    fudge in /opt/onbld/man manpages

--- tools/make_mandir.sh
+++ tools/make_mandir.sh
@@ -15,3 +15,10 @@ mkdir -p $PROTO || exit 1
 (cd $SRC && cp $DIR/make.rules . && $DIR/make rootdirs)
 (cd $SRC/man && cp $DIR/make.rules . && $DIR/make install)
 
+# fudge in the /opt/onbld/man manpages...
+rm -rf $PROTO/usr/share/man/man1onbld
+mkdir -p $PROTO/usr/share/man/man1onbld
+(cd $SRC/tools && find . -name \*.1 -exec cp {} \
+   $PROTO/usr/share/man/man1onbld \; )
+(cd $PROTO/usr/share/man/man1onbld &&
+  for f in *.1; do mv $f ${f}onbld; done)
